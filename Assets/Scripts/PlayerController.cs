b4258156db2e4e2e75aeea305c1f1d51be65cc37
Michael Arce <micharcesierra2028@gmail.com>
2020-02-10 19:38:45 -0500

    Primero Assets del proyecto IWBP

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool jump = false;
+    public static Vector3 v3;
+    public static bool isRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        v3 = gameObject.GetComponent<Transform>().position;
+        if (Input.GetKey("left"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-18000f * Time.deltaTime, 0));
+                isRight = false;
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(18000f * Time.deltaTime, 0));
+            isRight = true;
+        }
+        if (Input.GetKey("up") && jump)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5000f));
+            jump = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Platform")
+        {
+            jump = true;
+        }
+    }
+}
---
+++ Assets/Scripts/ShootController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    public GameObject shootPrefab;
+    public GameObject shootPrefabLeft;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = PlayerController.v3;
+        Quaternion q = new Quaternion();
+        if (Input.GetKeyUp(KeyCode.Space) && PlayerController.isRight)
+        {
+            Instantiate(shootPrefab,pos,q);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && !PlayerController.isRight)
+        {
+            Instantiate(shootPrefabLeft, pos, q);
+        }
+    }
+}
